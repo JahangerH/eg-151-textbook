8959812b72bee78a7d2c5fb56d56dcc33b43b3b0
JahangerH <hayder.jahanger@swansea.ac.uk>
2025-05-16 23:22:39 +0100

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -15,11 +15,11 @@ echo "Building PDF version of the book"
 quarto render book --to pdf
 
 echo "Copying PDF to OneDrive"
-cp _build/book/EG-151-Microcontrollers-2024-2025.pdf $HOME/'OneDrive - Swansea University/1 Projects/EG-151 Microcontrollers 2024-2025'
+cp _build/book/EG-151-Microcontrollers-2025-2026.pdf $HOME/'OneDrive - Swansea University/1 Projects/EG-151 Microcontrollers 2024-2025'
 # cp _build/book/EG-151-Microcontrollers-2024-2025.pdf website/_build/html
 
 echo
-echo "Updating GitHub pages https://cpjobling.github.io/eg-151-textbook"
+echo "Updating GitHub pages https://JahagngerH.github.io/eg-151-textbook"
 cp .gitignore .gitattributes _build/html # Allow large files
 ghp-import --message="Quarto version of GitHub pages website rebuilt" --no-jekyll --no-history --push --force _build/html 
 
