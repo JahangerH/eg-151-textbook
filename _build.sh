b4d3459c637ff496776399fe94cbc96f277886c5
JahangerH <hayder.jahanger@swansea.ac.uk>
2025-05-20 15:26:19 +0100

    Fix slides copy

--- _build.sh
+++ _build.sh
@@ -7,7 +7,8 @@ echo "Building website using Quarto book format"
 quarto render website --to html
 
 echo "Copying real lecture files into slides/"
-cp website/lectures/week0*/index.qmd slides/
+cp lectures/lecture*.qmd slides/
+
 
 echo "Building slides"
 quarto render slides --to revealjs
